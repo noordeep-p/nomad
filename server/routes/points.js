c16d678c18309a34213bb55acf34080f6208d7e6
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-02 15:39:57 -0500

    define routes for points CRUD operations

--- server/routes/points.js
+++ server/routes/points.js
@@ -1,9 +1,14 @@
-import express from "express";
+import express from 'express';
 
-import { createPoints, readPoints, updatePoints, deletePoints } from "../controllers/points.js";
+import {
+  createPoints, readPoints, updatePoints, deletePoints,
+} from '../controllers/points.js';
 
 const router = express.Router();
 
-router.get("/", readPoints);
+router.get('/', readPoints);
+router.post('/', createPoints);
+router.put('/', updatePoints);
+router.delete('/', deletePoints);
 
 export default router;
