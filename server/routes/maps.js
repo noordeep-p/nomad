f9dc465ec6ac9e1900c3b20c3700decbf77ccd10
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-02 15:40:14 -0500

    define routes for maps CRUD operations

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -1,9 +1,14 @@
-import express from "express";
+import express from 'express';
 
-import { getMaps } from "../controllers/maps.js";
+import {
+  createMaps, readMaps, updateMaps, deleteMaps,
+} from '../controllers/maps.js';
 
 const router = express.Router();
 
-router.get("/", getMaps);
+router.get('/', readMaps);
+router.post('/', createMaps);
+router.put('/', updateMaps);
+router.delete('/', deleteMaps);
 
 export default router;
