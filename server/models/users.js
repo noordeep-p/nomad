cb00fa9fbd8e88aabbe2ca9f73da012ef39f54f8
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-02 13:35:25 -0500

    setup user schema

---
+++ server/models/users.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const userModel = mongoose.model("userModel", userSchema);
+
+export default userModel;
