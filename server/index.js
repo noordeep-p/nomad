fb91281b6bf2068c379eb9322bfbc898dd3e8dc9
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-02 12:20:34 -0500

    setup mongoDB connection and use mount intial routes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,35 @@
-const express = require('express');
-const cors = require('cors');
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import userRoutes from "./routes/user.js";
+import mapRoutes from "./routes/maps.js";
+import pointRoutes from "./routes/maps.js";
 
 const app = express();
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 
-require('dotenv').config;
+app.use("/", userRoutes);
+app.use("/maps", mapRoutes);
+app.use("/points", pointRoutes);
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
-
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+  })
+  .catch((e) => {
+    console.log(e.message);
+  });
