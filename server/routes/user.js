ed6437e80cd572f7bfb0bccaf8697df2abccc6f2
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-02 15:40:54 -0500

    define routes for user CRUD operations

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,12 @@
-import express from "express";
+/* eslint-disable import/extensions */
+import express from 'express';
 
-import { loginUser, logoutUser, registerUser } from "../controllers/user.js";
+import { loginUser, logoutUser, registerUser } from '../controllers/user.js';
 
 const router = express.Router();
 
-router.get("/login", loginUser);
-router.get("/logout", logoutUser);
-router.get("/register", registerUser);
+router.get('/login', loginUser);
+router.get('/logout', logoutUser);
+router.get('/register', registerUser);
 
 export default router;
