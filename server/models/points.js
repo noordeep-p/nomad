a37ea61e27345526e07f56c724d00fe44df17634
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-03 00:07:12 -0500

    update points schema with type setting

--- server/models/points.js
+++ server/models/points.js
@@ -1,14 +1,38 @@
 import mongoose from 'mongoose';
 
-const pointSchema = mongoose.Schema({
-  name: String,
-  description: String,
-  image_url: String,
-  address: String,
-  latitude: Number,
-  longitude: Number,
-  map: String,
-});
+const pointSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image_url: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    lon: {
+      type: Number,
+      required: true,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+    map: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'MapModel',
+    },
+  },
+  { timestamps: true },
+);
 
 const PointModel = mongoose.model('PointModel', pointSchema);
 
