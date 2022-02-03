d8f30577b94fba8716c9cbcfa4acc1e9eee58459
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-03 00:05:14 -0500

    fix send status syntax error

--- server/controllers/maps.js
+++ server/controllers/maps.js
@@ -1,37 +1,38 @@
-import { mongoose } from 'mongoose';
 import MapModel from '../models/maps.js';
+
+const { mongoose } = 'mongoose';
 
 export const createMaps = async (req, res) => {
   const map = req.body;
   const newMap = new MapModel(map);
   try {
     await newMap.save();
-    res.status(201).json(newMap);
+    res.sendStatus(201).json(newMap);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.sendStatus(404).json({ message: error.message });
   }
 };
 
 export const readMaps = async (req, res) => {
   try {
     const allMaps = await MapModel.find();
-    res.status(200).json(allMaps);
+    res.sendStatus(200).json(allMaps);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.sendStatus(404).json({ message: error.message });
   }
 };
 
 export const updateMaps = async (req, res) => {
   const { id: _id } = req.params;
   const map = req.body;
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'No map with that ID' });
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.sendStatus(404).json({ message: 'No map with that ID' });
   const updatedMap = await MapModel.findByIdAndUpdate(_id, map, { new: true });
-  return res.status(201).json(updatedMap);
+  return res.sendStatus(201).json(updatedMap);
 };
 
 export const deleteMaps = async (req, res) => {
   const { id: _id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'No map with that ID' });
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.sendStatus(404).json({ message: 'No map with that ID' });
   await MapModel.findByIdAndDelete(_id);
-  return res.status(200).json({ message: 'Map deleted successfully' });
+  return res.sendStatus(200).json({ message: 'Map deleted successfully' });
 };
--- server/controllers/points.js
+++ server/controllers/points.js
@@ -1,37 +1,38 @@
-import { mongoose } from 'mongoose';
 import PointModel from '../models/points.js';
+
+const { mongoose } = 'mongoose';
 
 export const createPoints = async (req, res) => {
   const point = req.body;
   const newPoint = new PointModel(point);
   try {
     await newPoint.save();
-    res.status(201).json(newPoint);
+    res.sendStatus(201).json(newPoint);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.sendStatus(404).json({ message: error.message });
   }
 };
 
 export const readPoints = async (req, res) => {
   try {
     const allPoints = await PointModel.find();
-    res.status(200).json(allPoints);
+    res.sendStatus(200).json(allPoints);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.sendStatus(404).json({ message: error.message });
   }
 };
 
 export const updatePoints = async (req, res) => {
   const { id: _id } = req.params;
   const point = req.body;
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'No point with that ID' });
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.sendStatus(404).json({ message: 'No point with that ID' });
   const updatedPoint = await PointModel.findByIdAndUpdate(_id, point, { new: true });
-  return res.status(201).json(updatedPoint);
+  return res.sendStatus(201).json(updatedPoint);
 };
 
 export const deletePoints = async (req, res) => {
   const { id: _id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'No point with that ID' });
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.sendStatus(404).json({ message: 'No point with that ID' });
   await PointModel.findByIdAndDelete(_id);
-  return res.status(200).json({ message: 'Point deleted successfully' });
+  return res.sendStatus(200).json({ message: 'Point deleted successfully' });
 };
