b3665f583bc4f3cc39b2cbaa06c661d785a8607e
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-03 00:05:48 -0500

    add business logic for user routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,11 +1,24 @@
-export const loginUser = (req, res) => {
-  res.send('user login route');
+import UserModel from '../models/users.js';
+
+const { mongoose } = 'mongoose';
+
+export const readUser = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const userFound = await UserModel.findOne({ username });
+    res.sendStatus(200).json(userFound);
+  } catch (error) {
+    res.sendStatus(404).json({ message: error });
+  }
 };
 
-export const logoutUser = (req, res) => {
-  res.send('user logout route');
-};
-
-export const registerUser = (req, res) => {
-  res.send('user register route');
+export const createUser = async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  try {
+    await newUser.save();
+    res.sendStatus(201).json(newUser);
+  } catch (error) {
+    res.sendStatus(500).json({ message: error });
+  }
 };
