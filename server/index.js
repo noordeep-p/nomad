5243e859080037343d05c0ab3948af60f4d91539
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-06 22:49:25 -0500

    config dotenv path

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-dotenv.config();
+dotenv.config({ path: './.env.local' });
 
 const PORT = process.env.PORT || 3000;
 
