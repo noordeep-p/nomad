20942b9ba13a30de03b46ba5a789fa5e0ccb9c59
Olivia <olivia.wy.wong@gmail.com>
2022-02-07 16:25:04 -0800

    remove google react wrapper from package.json

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { Wrapper, Status } from '@googlemaps/react-wrapper';
 
 export default function Map() {
-  return <div>Individual Map</div>;
+  return <div>Inividual map</div>;
 }
