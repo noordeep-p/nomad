f2031de86a85bb4f7974b1fb64814917f8111918
Olivia <olivia.wy.wong@gmail.com>
2022-02-07 19:12:12 -0800

    add GoogleMapReact component and render to map page

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,5 +1,25 @@
 /* eslint-disable no-unused-vars */
+import React from 'react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import GoogleMapReact from 'google-map-react';
 
 export default function Map() {
-  return <div>Inividual map</div>;
+  const coords = { lat: 0, lng: 0 };
+  return (
+    <div className="mapContainer">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+        }}
+        // onChange={''}
+        // onChildClick={''}
+      />
+    </div>
+  );
 }
