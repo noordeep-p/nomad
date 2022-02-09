ab9de81d02b357563f74f9a3623e33ac46f6f441
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-08 22:59:57 -0500

    run linter and apply fixes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,58 +3,49 @@ import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import UserModel from '../models/users.js';
 
-const { mongoose } = 'mongoose';
-dotenv.config();
+dotenv.config({ path: '../.env.local' });
 
 export const readUser = async (req, res) => {
   const { username } = req.params;
   try {
     const userFound = await UserModel.findOne({ username });
-    res.status(200).json(userFound);
+    return res.status(200).json(userFound);
   } catch (error) {
-    res.status(404).json({ message: error });
+    return res.status(404).json({ message: error });
   }
 };
 
 export const login = async (req, res) => {
-  console.log(req.body)
   try {
     const { email, password } = req.body;
     if (!email || !password) {
       return res.status(400).json({ msg: 'Please enter all fields' });
     }
-
     const user = await UserModel.findOne({ email });
     if (!user) {
       return res.status(400).json({ msg: 'Account has not been registered' });
     }
-
     const userFound = await bcrypt.compare(password, user.password);
-
     if (!userFound) {
       return res.status(400).json({ msg: 'Invalid password' });
     }
-
     const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET);
-    res.json({ accessToken });
+    return res.json({ accessToken });
   } catch (error) {
-    res.status(404).json({ message: error });
+    return res.status(404).json({ message: error });
   }
 };
 
 export const createUser = async (req, res) => {
   const { username, email, password } = req.body;
-  console.log(req.body)
   try {
     if (!username || !email || !password) {
       return res.status(400).json({ msg: 'Please enter all fields' });
     }
-
     const existingUser = await UserModel.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ msg: 'This email already existed, please Login' });
+      return res.status(400).json({ msg: 'This email already existed, please login' });
     }
-
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
     const newUser = new UserModel({
@@ -62,10 +53,9 @@ export const createUser = async (req, res) => {
       email,
       password: passwordHash,
     });
-
     await newUser.save();
-    res.status(201).json(newUser);
+    return res.status(201).json(newUser);
   } catch (error) {
-    res.status(500).json({ message: error });
+    return res.status(500).json({ message: error });
   }
 };
