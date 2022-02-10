f7824fcc6a59faede08363bbc6d763d11fe9ce11
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-09 21:38:59 -0500

    add path to dotenv config

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,24 +1,23 @@
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 
-const { mongoose } = 'mongoose';
-dotenv.config();
+dotenv.config({ path: '../.env.local' });
 
 export default function authenticateToken(req, res, next) {
   try {
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1];
     if (!token) {
-      return res.status(403).json({ msg: 'Authorization denied' });
+      return res.status(403).json({ msg: 'No JWT Token: Authorization denied' });
     }
 
     const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     if (!verified) {
-      return res.status(403).json({ msg: 'Authorization denied' });
+      return res.status(403).json({ msg: 'Cannot Verify JWT Token: Authorization denied' });
     }
     req.user = verified.user;
-    next();
+    return next();
   } catch (error) {
-    res.status(500).json({ message: error });
+    return res.status(500).json({ message: error });
   }
 }
