012202050d4c7c9eb456699fc6b91575617b218b
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-10 23:24:07 -0500

    remove unused imports

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Grid } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import NavBar from './NavBar';
 import HomePage from './HomePage';
 import MyMaps from './MyMaps';
@@ -10,36 +8,13 @@ import FavoriteMaps from './FavoriteMaps';
 import Map from './Map';
 import Login from './Login';
 import Register from './Register';
-import MapFeed from './components/MapFeed';
-import RightBar from './components/RightBar';
-import LeftBar from './components/LeftBar';
 import useLocalStorage from '../hooks/useLocalStorage';
 
-const useStyles = makeStyles((theme) => ({
-  right: {
-    [theme.breakpoints.down('sm')]: {
-      display: 'none',
-    },
-  },
-}));
-
 export default function App() {
-  const classes = useStyles();
   const [accessToken, setAccessToken] = useLocalStorage('accessToken', '');
   return (
     <Router>
       <NavBar accessToken={accessToken} setAccessToken={setAccessToken} />
-      <Grid container>
-        <Grid item sm={2} xs={2}>
-          <Leftbar />
-        </Grid>
-        <Grid item sm={7} xs={10}>
-          <Feed />
-        </Grid>
-        <Grid item sm={3} className={classes.right}>
-          <Rightbar />
-        </Grid>
-      </Grid>
       <Switch>
         <Route exact path="/mymaps">
           <MyMaps />
