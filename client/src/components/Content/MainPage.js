acc6de687539639ef5b81878569153015a494a1d
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 17:45:02 -0500

    create MUI container component on main page to display shared maps

---
+++ client/src/components/Content/MainPage.js
@@ -0,0 +1,174 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-no-useless-fragment */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
+import InputBase from '@material-ui/core/InputBase';
+
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import ModeOfTravelIcon from '@mui/icons-material/ModeOfTravel';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import LogoutIcon from '@mui/icons-material/Logout';
+import ChatIcon from '@mui/icons-material/Chat';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+import useStyles from './StylesMain';
+
+export default function MainPage(props) {
+  const classes = useStyles();
+
+  const content = {
+    link1: 'Explore',
+    link2: 'Your Favorites',
+    link3: 'Chats',
+    link4: 'Logout',
+    ...props.content,
+  };
+
+  const buckets = {
+    main: Array.isArray(props.bucketMain) ? props.bucketMain : [],
+  };
+
+  const [state, setState] = React.useState({ open: false });
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === 'keydown'
+      && (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, open });
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Link
+            href="/"
+            variant="h5"
+            color="inherit"
+            underline="none"
+            className={classes.linkBrand}
+          >
+            <ModeOfTravelIcon />
+            {' '}
+            Nomad
+          </Link>
+          <Link
+            href="/"
+            variant="h5"
+            color="inherit"
+            underline="none"
+            className={classes.linkBrandSmall}
+          >
+            <ModeOfTravelIcon />
+            {' '}
+            Nomad
+          </Link>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Drawer className={classes.drawer} variant="permanent">
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            <ListItem button key={content.link1}>
+              <ListItemIcon>
+                <LocationOnIcon />
+              </ListItemIcon>
+              <ListItemText primary={content.link1} />
+            </ListItem>
+            <ListItem button key={content.link2}>
+              <ListItemIcon>
+                <FavoriteIcon />
+              </ListItemIcon>
+              <ListItemText primary={content.link2} />
+            </ListItem>
+            <ListItem button key={content.link3}>
+              <ListItemIcon>
+                <ChatIcon />
+              </ListItemIcon>
+              <ListItemText primary={content.link3} />
+            </ListItem>
+            <ListItem button key={content.link4}>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary={content.link4} />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+      <Drawer anchor="left" open={state.open} onClose={toggleDrawer(false)}>
+        <div className={classes.drawerContainer}>
+          <List>
+            <ListItem button key={content.link1}>
+              <ListItemIcon>
+                <LocationOnIcon />
+              </ListItemIcon>
+              <ListItemText primary={content.link1} />
+            </ListItem>
+            <ListItem button key={content.link2}>
+              <ListItemIcon>
+                <FavoriteIcon />
+              </ListItemIcon>
+              <ListItemText primary={content.link2} />
+            </ListItem>
+            <ListItem button key={content.link3}>
+              <ListItemIcon>
+                <ChatIcon />
+              </ListItemIcon>
+              <ListItemText primary={content.link3} />
+            </ListItem>
+            <ListItem button key={content.link4}>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary={content.link4} />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+      <main className={classes.content}>
+        <Toolbar />
+        <div>
+          {buckets.main.map((component) => (
+            <>{component}</>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
