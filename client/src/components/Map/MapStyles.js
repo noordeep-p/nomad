c46bbeaac7a9f18839fa971c7fe5ef98cc37d14c
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 17:40:26 -0500

    add styling to maps to disable showing of labels and pins

---
+++ client/src/components/Map/MapStyles.js
@@ -0,0 +1,122 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const mapStyles = [
+  {
+    featureType: 'administrative',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+      {
+        lightness: '12',
+      },
+    ],
+  },
+];
+
+export const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: '10px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '100px',
+  },
+  mapContainer: {
+    height: '85vh',
+    width: '100%',
+  },
+  markerContainer: {
+    position: 'absolute',
+    transform: 'translate(-50%, -50%)',
+    zIndex: 1,
+    '&:hover': { zIndex: 2 },
+  },
+  pointer: {
+    cursor: 'pointer',
+  },
+  chip: {
+    margin: '5px 5px 5px 0',
+  },
+  subtitle: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: '10px',
+  },
+  spacing: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    marginBottom: '30px',
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  loading: {
+    height: '600px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    padding: '25px',
+  },
+  marginBottom: {
+    marginBottom: '30px',
+  },
+  list: {
+    height: '75vh',
+    overflow: 'auto',
+  },
+}));
