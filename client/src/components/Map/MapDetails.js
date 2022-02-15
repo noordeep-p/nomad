393be9563d97172e21601a9ce48f0778d5fe19c9
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 17:31:02 -0500

    create MUI sidebar and cards to display map pin details

---
+++ client/src/components/Map/MapDetails.js
@@ -0,0 +1,93 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  Chip,
+} from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Rating from '@material-ui/lab/Rating';
+import AddIcon from '@mui/icons-material/Add';
+
+import { useStyles } from './MapStyles';
+
+export default function MapDetails({ place, selected, refProp }) {
+  if (selected) {
+    refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+  const classes = useStyles();
+
+  return (
+    <Card elevation={6} style={{ display: 'inline-flex' }}>
+      <CardMedia
+        style={{ height: '45vh', width: 200 }}
+        image={
+          place.photo
+            ? place.photo.images.large.url
+            : 'https://images.unsplash.com/photo-1580793241553-e9f1cce181af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80'
+        }
+        title={place.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          {place.name}
+        </Typography>
+        <Box display="flex" justifyContent="space-between" my={2}>
+          <Rating name="read-only" value={Number(place.rating)} readOnly />
+          <Typography component="legend">
+            {place.num_reviews} review
+            {place.num_reviews > 1 && 's'}
+          </Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Typography component="legend">Ranking</Typography>
+          <Typography gutterBottom variant="subtitle1">
+            {place.ranking}
+          </Typography>
+        </Box>
+        {place?.cuisine?.map(({ name }) => (
+          <Chip key={name} size="small" label={name} className={classes.chip} />
+        ))}
+        {place.address && (
+          <Typography
+            gutterBottom
+            variant="body2"
+            color="textSecondary"
+            className={classes.subtitle}
+          >
+            <LocationOnIcon />
+            {place.address}
+          </Typography>
+        )}
+        <Box display="flex" flexDirection="row" justifyContent="space-evenly">
+          <Button
+            variant="contained"
+            color="green"
+            style={{
+              backgroundColor: 'gray',
+              color: 'white',
+            }}
+          >
+            Website
+          </Button>
+          <Button
+            variant="contained"
+            color="green"
+            style={{
+              backgroundColor: 'green',
+              color: 'white',
+            }}
+            onClick={() => window.open(place.website, '_blank')}
+          >
+            <AddIcon />
+            Add Pin
+          </Button>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
