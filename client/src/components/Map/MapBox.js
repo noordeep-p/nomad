462a146c3b120f710b5fbbfcbcafb6aee3dafe5a
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 17:29:56 -0500

    create container and initialize google map react

---
+++ client/src/components/Map/MapBox.js
@@ -0,0 +1,73 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import { useStyles, mapStyles } from './MapStyles';
+
+export default function MapBox({
+  coords,
+  places,
+  setCoords,
+  setBounds,
+  setChildClicked,
+}) {
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
+        onChange={(e) => {
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClicked(child)}
+      >
+        {places.length
+          && places.map((place, i) => (
+            <div
+              className={classes.markerContainer}
+              lat={Number(place.latitude)}
+              lng={Number(place.longitude)}
+              key={i}
+            >
+              {!matches ? (
+                <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              ) : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography
+                    className={classes.typography}
+                    variant="captiontext"
+                    gutterBottom
+                  >
+                    {' '}
+                    {place.name}
+                  </Typography>
+                  <img
+                    className={classes.pointer}
+                    src={
+                      place.photo
+                        ? place.photo.images.large.url
+                        : 'https://images.unsplash.com/photo-1580793241553-e9f1cce181af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80'
+                    }
+                  />
+                </Paper>
+              )}
+            </div>
+          ))}
+      </GoogleMapReact>
+    </div>
+  );
+}
