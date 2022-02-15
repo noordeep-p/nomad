a8c039a7b8e556923062375b291bdcba11842662
Olivia <olivia.wy.wong@gmail.com>
2022-02-15 15:46:45 -0800

    setup layout for Chatroom to join chat

--- client/src/components/Chat/Chatroom.js
+++ client/src/components/Chat/Chatroom.js
@@ -1,9 +1,29 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 export default function Chatroom() {
   return (
     <div>
-      Chatroom
+      <Card sx={{ width: '25%', height: '30vh' }}>
+        <CardContent>
+          <Typography variant="h4" component="div">
+            Start a Chat
+          </Typography>
+          <Typography variant="h3">
+            <input type="text" placeholder="Enter Your Name" />
+            <br />
+            <input type="text" placeholder="Enter Room ID" />
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <button variant="contained">Start Chat</button>
+        </CardActions>
+      </Card>
+
     </div>
   );
 }
