9a4de257612eb680b727564626b654aae9a665bc
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 17:07:19 -0500

    create new file structure to store frontend components

--- client/src/components/Marker.js
+++
@@ -1,7 +0,0 @@
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-
-export default function Marker() {
-  return (
-    <LocationOnIcon color="primary" />
-  );
-}
--- client/src/components/NewMap.js
+++
@@ -1,3 +0,0 @@
-export default function NewMap() {
-  return <div>New Map!</div>;
-}
