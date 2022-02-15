ea63585472d789ce88adf8b3f62839cca0a96881
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 17:26:35 -0500

    add helper function to pull places data from places rapid API

---
+++ client/src/helpers/rapidAPI.js
@@ -0,0 +1,33 @@
+/* eslint-disable consistent-return */
+// All external APIs used when a user creates a map are loaded here
+
+import axios from 'axios';
+
+// Load places data from tripadvisor api
+const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+          'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+        },
+      },
+    );
+
+    return data;
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+export default getPlacesData;
