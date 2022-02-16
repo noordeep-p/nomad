affc60783d73b3ad3037850ca25520c9458766c7
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 21:57:45 -0500

    remove unused controllers

--- server/controllers/maps.js
+++ server/controllers/maps.js
@@ -3,11 +3,8 @@
 /* eslint-disable no-underscore-dangle */
 import mongoose from 'mongoose';
 import MapModel from '../models/maps.js';
-import PointModel from '../models/points.js';
 
-// const { mongoose } = 'mongoose';
-
-export const readMaps = async (req, res) => {
+export const readAllMaps = async (req, res) => {
   try {
     const allMaps = await MapModel.find();
     console.log(allMaps);
@@ -28,46 +25,12 @@ export const readSingleMap = async (req, res) => {
   }
 };
 
-export const readMapSinglePoint = async (req, res) => {
-  try {
-    const { pointId: _id } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'This map does not contain this point' });
-    const point = await PointModel.find({ _id });
-    res.status(200).json(point);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
-
-export const readMapAllPoint = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const allPoint = await PointModel.find({ map: id });
-    res.status(200).json(allPoint);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
-
 export const createMaps = async (req, res) => {
   const map = req.body;
   const newMap = new MapModel(map);
   try {
     await newMap.save();
     res.status(201).json(newMap);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
-
-export const createMapPoint = async (req, res) => {
-  const point = req.body;
-  const newPoint = new PointModel(point);
-
-  try {
-    await newPoint.save();
-    await MapModel.findOneAndUpdate(newPoint.map, { $push: { points: newPoint._id } }, { new: true });
-    res.status(201).json(newPoint);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -81,33 +44,9 @@ export const updateMaps = async (req, res) => {
   return res.status(201).json(updatedMap);
 };
 
-export const updateMapSinglePoint = async (req, res) => {
-  const { pointId: _id } = req.params;
-  const point = req.body;
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'This map does not contain this point' });
-  const updatedMapSinglePoint = await PointModel.findByIdAndUpdate(_id, point, { new: true });
-  return res.status(201).json(updatedMapSinglePoint);
-};
-
 export const deleteMaps = async (req, res) => {
   const { id: _id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'No map with that ID' });
   await MapModel.findByIdAndDelete(_id);
   return res.status(200).json({ message: 'Map deleted successfully' });
-};
-
-export const deleteMapSinglePoint = async (req, res) => {
-  const { pointId: _id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'This map does not contain any point' });
-  await PointModel.findByIdAndDelete(_id);
-  await MapModel.findOneAndUpdate(_id, { $pull: { points: _id } }, { new: true });
-  return res.status(200).json({ message: 'Point deleted successfully' });
-};
-
-export const deleteMapAllPoint = async (req, res) => {
-  const { id: _id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'No map with that ID' });
-  await PointModel.findByIdAndDelete(_id);
-  await MapModel.findOneAndUpdate(_id, { $set: { points: [] } }, { new: true });
-  return res.status(200).json({ message: 'Point deleted successfully' });
 };
