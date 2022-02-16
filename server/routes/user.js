159fbde2c0734d191a069d531346fdcace4dfc03
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 21:59:38 -0500

    remove dead code

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,15 +1,13 @@
 import express from 'express';
 
 import {
-  readUser, login, createUser,
+  readUser, login, register,
 } from '../controllers/user.js';
 
 const router = express.Router();
 
 router.get('/:username', readUser);
 router.post('/login', login);
-router.post('/create', createUser);
-// router.get('/:id/maps', readUserAllMap); => move to maps route
-// router.get('/:id/:mapId', readUserSingleMap); => move to maps route
+router.post('/register', register);
 
 export default router;
