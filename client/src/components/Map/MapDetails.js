b16f51b172a7deec90662e72ce61593a5fc725c1
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 19:50:38 -0500

    create function to pull data onClick of "addPin" button

--- client/src/components/Map/MapDetails.js
+++ client/src/components/Map/MapDetails.js
@@ -21,6 +21,23 @@ export default function MapDetails({ place, selected, refProp }) {
   }
   const classes = useStyles();
 
+  const savePin = (placeData) => {
+    const image = placeData.photo ? placeData.photo.images.large.url : 'https://images.unsplash.com/photo-1580793241553-e9f1cce181af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80';
+    const data = {
+      photo: image,
+      name: placeData.name,
+      num_reviews: placeData.num_reviews,
+      cuisine: placeData.cuisine,
+      address: placeData.address,
+      website: placeData.website,
+      ranking: placeData.ranking,
+      rating: placeData.rating,
+      lat: placeData.latitude,
+      lng: placeData.longitude,
+    };
+    console.log(data);
+  };
+
   return (
     <Card elevation={6} style={{ display: 'inline-flex' }}>
       <CardMedia
@@ -66,22 +83,21 @@ export default function MapDetails({ place, selected, refProp }) {
         <Box display="flex" flexDirection="row" justifyContent="space-evenly">
           <Button
             variant="contained"
-            color="green"
             style={{
               backgroundColor: 'gray',
               color: 'white',
             }}
+            onClick={() => window.open(place.website, '_blank')}
           >
             Website
           </Button>
           <Button
             variant="contained"
-            color="green"
             style={{
               backgroundColor: 'green',
               color: 'white',
             }}
-            onClick={() => window.open(place.website, '_blank')}
+            onClick={() => savePin(place)}
           >
             <AddIcon />
             Add Pin
