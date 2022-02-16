0a31a794b70d1b8bbca09402e35ce287916ed4bf
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 21:59:27 -0500

    delete unused points routes

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -2,21 +2,16 @@
 import express from 'express';
 
 import {
-  readMaps, readSingleMap, readMapSinglePoint, readMapAllPoint, createMaps, createMapPoint, updateMaps, deleteMaps, updateMapSinglePoint, deleteMapSinglePoint, deleteMapAllPoint,
+  readAllMaps, readSingleMap, createMaps, updateMaps, deleteMaps,
 } from '../controllers/maps.js';
 
 const router = express.Router();
 
-router.get('/', readMaps);
+router.get('/', readAllMaps);
+router.post('/', createMaps);
+// should have user profile
 router.get('/:id', readSingleMap);
-router.get('/:id/:pointId', readMapSinglePoint);
-router.get('/:id/points', readMapAllPoint);
-router.post('/', createMaps); // should have user profile
-router.post('/:id/points', createMapPoint); // should have user profile
-router.patch('/:id', updateMaps); // should have user profile
-router.patch('/:id/:pointId', updateMapSinglePoint); // should have user profile
-router.delete('/:id', deleteMaps); // should have user profile
-router.delete('/:id/:pointId', deleteMapSinglePoint); // should have user profile
-router.delete('/:id/points', deleteMapAllPoint); // should have user profile
+router.patch('/:id', updateMaps);
+router.delete('/:id', deleteMaps);
 
 export default router;
