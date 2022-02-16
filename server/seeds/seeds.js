85600476e81d34194bfcdb0d81f668c7c05ca9d5
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-15 22:00:38 -0500

    remove unused imports

--- server/seeds/seeds.js
+++ server/seeds/seeds.js
@@ -2,18 +2,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import UserModel from '../models/users.js';
 import MapModel from '../models/maps.js';
-import PointModel from '../models/points.js';
 import { generateMaps, getMapIds } from './mapSeeds.js';
 import { generateUsers, getUserIds } from './userSeeds.js';
-import { generatePoints, getPointIds } from './pointSeeds.js';
 
-const pointIds = getPointIds();
 const userIds = getUserIds();
 const mapIds = getMapIds();
 
 const userSeeds = generateUsers(userIds);
-const pointSeeds = generatePoints(pointIds, mapIds);
-const mapSeeds = generateMaps(mapIds, userIds, pointIds);
+const mapSeeds = generateMaps(mapIds, userIds);
 
 dotenv.config({ path: './.env.local' });
 
@@ -31,10 +27,8 @@ mongoose
 
 Promise.all([
   UserModel.collection.drop(),
-  PointModel.collection.drop(),
   MapModel.collection.drop(),
   UserModel.insertMany(userSeeds),
-  PointModel.insertMany(pointSeeds),
   MapModel.insertMany(mapSeeds),
 ]).then(() => {
   mongoose.connection.close();
