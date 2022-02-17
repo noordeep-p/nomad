1738ed1a91904e46aa407076f1e6f4528b1b5f72
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-16 23:33:10 -0500

    set userContext and pass current user to all components

--- client/src/App.js
+++ client/src/App.js
@@ -12,43 +12,52 @@ import Chatroom from './components/Chat/Chatroom';
 import MapIndex from './components/Map/MapIndex';
 
 import useLocalStorage from './hooks/useLocalStorage';
+import { UserContext } from './userContext';
 
 export default function App() {
   const [accessToken, setAccessToken] = useLocalStorage('accessToken', null);
+  const currentUser = localStorage.getItem('username');
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/login">
-          <UserAuth setAccessToken={setAccessToken} />
-        </Route>
-        <Route exact path="/">
-          {accessToken ? (
-            <MainPage
-              setAccessToken={setAccessToken}
-              bucketMain={[
-                <MapCard
-                  content={{
-                    'primary-action': 'View this itinerary',
-                  }}
-                />,
-              ]}
-            />
-          ) : (
-            <Redirect to="/login" />
-          )}
-        </Route>
-        <Route exact path="/map">
-          {accessToken ? <MapIndex /> : <Redirect to="/login" />}
-        </Route>
-        <Route exact path="/chatroom">
-          {accessToken ? (
-            <Chatroom setAccessToken={setAccessToken} />
-          ) : (
-            <Redirect to="/login" />
-          )}
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext>
+      <Router>
+        <Switch>
+          <Route exact path="/login">
+            <UserAuth setAccessToken={setAccessToken} />
+          </Route>
+          <Route exact path="/">
+            {accessToken ? (
+              <MainPage
+                setAccessToken={setAccessToken}
+                currentUser={currentUser}
+                bucketMain={[
+                  <MapCard
+                    content={{
+                      'primary-action': 'View this itinerary',
+                    }}
+                  />,
+                ]}
+              />
+            ) : (
+              <Redirect to="/login" />
+            )}
+          </Route>
+          <Route exact path="/map">
+            {accessToken ? (
+              <MapIndex currentUser={currentUser} />
+            ) : (
+              <Redirect to="/login" />
+            )}
+          </Route>
+          <Route exact path="/chatroom">
+            {accessToken ? (
+              <Chatroom currentUser={currentUser} />
+            ) : (
+              <Redirect to="/login" />
+            )}
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext>
   );
 }
