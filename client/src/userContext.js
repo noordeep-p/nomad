6f9017a8c03778f5a803f5729bddd24cd23635d4
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-16 23:33:48 -0500

    create file to hold logic for global user context

---
+++ client/src/userContext.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+import React, { useContext, useState } from 'react';
+
+const userContext = React.createContext();
+const userUpdateContext = React.createContext();
+
+export function currentUser() {
+  return useContext(userContext);
+}
+
+export function userUpdate() {
+  return useContext(userUpdateContext);
+}
+
+export function UserContext({ children }) {
+  const [user, setUser] = useState();
+
+  function updateUser(data) {
+    if (data) {
+      setUser(data.username);
+      localStorage.setItem('username', data.username);
+    }
+  }
+
+  return (
+    <userContext.Provider value={user}>
+      <userUpdateContext.Provider value={updateUser}>
+        {children}
+      </userUpdateContext.Provider>
+    </userContext.Provider>
+  );
+}
