4ff7ea7a667751b4b9ca8dc8edaaa19db1a3f719
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-16 19:09:11 -0500

    protect private user routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import MainPage from './components/Content/MainPage';
 import MapCard from './components/Content/MapCard';
 import UserAuth from './components/Content/UserAuth';
@@ -14,25 +19,33 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/map">
-          <MapIndex />
-        </Route>
         <Route exact path="/login">
           <UserAuth accessToken={accessToken} setAccessToken={setAccessToken} />
         </Route>
-        <Route exact path="/chatroom">
-          <Chatroom setAccessToken={setAccessToken} />
-        </Route>
         <Route exact path="/">
-          <MainPage
-            bucketMain={[
-              <MapCard
-                content={{
-                  'primary-action': 'View this itinerary',
-                }}
-              />,
-            ]}
-          />
+          {accessToken ? (
+            <MainPage
+              bucketMain={[
+                <MapCard
+                  content={{
+                    'primary-action': 'View this itinerary',
+                  }}
+                />,
+              ]}
+            />
+          ) : (
+            <Redirect to="/login" />
+          )}
+        </Route>
+        <Route exact path="/map">
+          {accessToken ? <MapIndex /> : <Redirect to="/login" />}
+        </Route>
+        <Route exact path="/chatroom">
+          {accessToken ? (
+            <Chatroom setAccessToken={setAccessToken} />
+          ) : (
+            <Redirect to="/login" />
+          )}
         </Route>
       </Switch>
     </Router>
