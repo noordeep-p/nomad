92852ff37739958a135bf0f9fd39140624084123
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-17 19:50:10 -0500

    add avatar photo and name to chatroom model

--- server/models/chatroom.js
+++ server/models/chatroom.js
@@ -2,8 +2,15 @@ import mongoose from 'mongoose';
 
 const ChatroomSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+    },
+    photo: {
+      type: String,
+    },
     members: {
       type: Array,
+      default: [],
     },
   },
   { timestamps: true },
