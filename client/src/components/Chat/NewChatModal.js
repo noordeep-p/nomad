fbb8bffaeeaf07b4e8c8598bd9a66f9d15605ecc
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-18 16:14:55 -0500

    create modal for new chatroom form

---
+++ client/src/components/Chat/NewChatModal.js
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import axios from 'axios';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function NewChatModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleNewChat = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log(data.get('name'), data.get('photo'));
+    axios
+      .post('http://localhost:8000/chatroom', {
+        name: data.get('name'),
+        photo: data.get('photo'),
+      })
+      .then((res) => {
+        if (res) {
+          console.log(res.data);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Create New Chatroom</Button>
+      <Modal
+        aria-labelledby="transition-modal-name"
+        aria-describedby="transition-modal-photo"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box
+            sx={style}
+            component="form"
+            noValidate
+            onSubmit={(event) => handleNewChat(event)}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="name"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="photo"
+                  label="Image URL"
+                  name="photo"
+                  autoComplete="photo"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onClick={handleClose}
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
