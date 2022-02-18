e4cb1e97ef68d0f7e623dfb51ce0f91b5becc7e3
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-17 19:51:00 -0500

    correct function name to match import

--- server/routes/chatroom.js
+++ server/routes/chatroom.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
 import {
-  createChatroom, getChatroomsWithUser, getAllChatroomMembers,
+  createChatroom, getChatroomsWithUser, getAllChatrooms,
 } from '../controllers/chatroom.js';
 
 const router = express.Router();
 
 router.post('/', createChatroom);
-router.get('/', getAllChatroomMembers);
+router.get('/', getAllChatrooms);
 router.get('/:userId', getChatroomsWithUser);
 
 export default router;
