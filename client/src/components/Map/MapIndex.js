684a9ac560f5b49d602df754fef123cc2233d58a
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-19 18:45:38 -0500

    display added pins at the end of the map page

--- client/src/components/Map/MapIndex.js
+++ client/src/components/Map/MapIndex.js
@@ -1,5 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Grid } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import {
+  Box,
+  Grid,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+  Chip,
+  Button,
+} from '@material-ui/core';
+
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Rating from '@material-ui/lab/Rating';
 
 import getPlacesData from '../../helpers/rapidAPI';
 
@@ -7,7 +21,15 @@ import MapFilters from './MapFilters';
 import MapBox from './MapBox';
 import MapList from './MapList';
 
+import { useStyles } from './MapStyles';
+
+const urlParams = window.location.pathname.slice(5);
+
 export default function MapIndex() {
+  const history = useHistory();
+
+  const classes = useStyles();
+
   // All Maps hooks //
 
   // google maps hooks
@@ -24,6 +46,8 @@ export default function MapIndex() {
   // map search types
   const [type, setType] = useState('attractions');
   const [rating, setRating] = useState('');
+  // added pins
+  const [savedPins, setSavedPins] = useState([]);
 
   // Add useEffect hooks to manage all side effects //
 
@@ -62,6 +86,18 @@ export default function MapIndex() {
     setCoords({ lat, lng });
   };
 
+  const handleMapSubmit = () => {
+    axios
+      .patch(`http://localhost:8000/maps/${urlParams}`, { savedPins })
+      .then((res) => {
+        if (res) {
+          console.log(res.data);
+          history.push('/');
+        }
+      })
+      .catch((e) => console.error(e));
+  };
+
   return (
     <>
       <MapFilters
@@ -72,13 +108,36 @@ export default function MapIndex() {
         onLoad={onLoad}
         onPlaceChanged={onPlaceChanged}
       />
-      <Box mt={8}>
-        <Grid container spacing={3} style={{ width: '100%', mt: '50px' }}>
+      <Box
+        mt={10}
+        display="inline"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h5">Ready to share your adventures?</Typography>
+        <Button
+          variant="contained"
+          size="large"
+          color="primary"
+          style={{
+            margin: '2em',
+          }}
+          onClick={handleMapSubmit}
+        >
+          Upload Itinerary
+        </Button>
+      </Box>
+      <Box mt={4}>
+        <Grid container spacing={2} style={{ width: '100%', mt: '50px' }}>
           <Grid item xs={12} md={4}>
             <MapList
               isLoading={isLoading}
               childClicked={childClicked}
               places={filteredPlaces.length ? filteredPlaces : places}
+              setSavedPins={setSavedPins}
             />
           </Grid>
           <Grid
@@ -102,6 +161,75 @@ export default function MapIndex() {
             />
           </Grid>
         </Grid>
+        <Box m={6}>
+          <Grid container style={{ width: '100%' }}>
+            <Grid item md={12}>
+              <Typography variant="h6">
+                Places Added to your Shared Itinerary:
+              </Typography>
+              {savedPins.map((place) => (
+                <Card
+                  elevation={6}
+                  style={{
+                    display: 'inline-flex',
+                    maxWidth: '28vw',
+                    maxHeight: 'fit-content',
+                    margin: '1vw',
+                  }}
+                >
+                  <CardMedia
+                    style={{ height: 'inherit', minWidth: '10vw' }}
+                    image={place.photo}
+                    title={place.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h6">
+                      {place.name}
+                    </Typography>
+                    <Box display="flex" justifyContent="space-between" my={2}>
+                      <Rating
+                        name="read-only"
+                        value={Number(place.rating)}
+                        readOnly
+                      />
+                      <Typography component="legend">
+                        {place.num_reviews}
+                        {' '}
+                        review
+                        {place.num_reviews > 1 && 's'}
+                      </Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="space-between">
+                      <Typography component="legend">Ranking</Typography>
+                      <Typography gutterBottom variant="caption">
+                        {place.ranking}
+                      </Typography>
+                    </Box>
+                    {place?.cuisine?.map(({ name }) => (
+                      <Chip
+                        key={name}
+                        size="small"
+                        label={name}
+                        className={classes.chip}
+                      />
+                    ))}
+                    {place.address && (
+                      <Typography
+                        gutterBottom
+                        variant="body2"
+                        color="textSecondary"
+                        className={classes.subtitle}
+                      >
+                        <LocationOnIcon />
+                        {place.address}
+                      </Typography>
+                    )}
+                  </CardContent>
+                </Card>
+              ))}
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
     </>
   );
