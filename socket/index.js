637dd5948442730c1931ce1fe5c2faf911302546
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-18 20:20:21 -0500

    update socket.io logic

--- socket/index.js
+++ socket/index.js
@@ -5,21 +5,14 @@ const io = require("socket.io")(4000, {
 });
 
 io.on("connection", (socket) => {
-  console.log("New Connection...");
-
-  socket.emit("message", "Welcome!");
-
-  socket.broadcast.emit("message", "user joined");
-
-  socket.on("join_chat", (data) => {
-    socket.join(data);
+  console.log("a user connected.");
+  socket.on('message', () => {
+    socket.broadcast.emit("message", "message");
   });
-
-  socket.on("send_messageData", (data) => {
-    socket.to(data.chatroom).emit("receive_messageData", data);
+  socket.on('chat', () => {
+    socket.broadcast.emit("chat", "chat");
   });
-
   socket.on("disconnect", () => {
-    console.log(`${socket.id} disconnected`);
+    console.log("a user disconnected!");
   });
 });
