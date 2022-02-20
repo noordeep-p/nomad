dd487201d86045932d6c5ae411465b0e32a67f88
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-19 23:08:07 -0500

    create controller to get all maps created by current user

--- server/controllers/maps.js
+++ server/controllers/maps.js
@@ -7,7 +7,6 @@ import MapModel from '../models/maps.js';
 export const readAllMaps = async (req, res) => {
   try {
     const allMaps = await MapModel.find();
-    console.log(allMaps);
     res.status(200).json(allMaps);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -18,7 +17,6 @@ export const readSingleMap = async (req, res) => {
   const { id } = req.params;
   try {
     const map = await MapModel.find({ _id: id });
-    console.log(map);
     res.status(200).json(map);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -48,4 +46,14 @@ export const deleteMaps = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: 'No map with that ID' });
   await MapModel.findByIdAndDelete(_id);
   return res.status(200).json({ message: 'Map deleted successfully' });
+};
+
+export const getAllUserMaps = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const map = await MapModel.find({ owner: { $in: mongoose.Types.ObjectId(id) } });
+    res.status(200).json(map);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
