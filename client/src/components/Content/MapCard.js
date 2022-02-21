8b6a624b9e0b1902c7b6e775891fb6149d4ab9b0
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-02-20 22:27:32 -0500

    display point images on the home page map cards

--- client/src/components/Content/MapCard.js
+++ client/src/components/Content/MapCard.js
@@ -34,94 +34,109 @@ const useStyles = makeStyles((theme) => ({
 
 export default function MapCard(props) {
   const classes = useStyles();
-  const mapCards = props.cards.reverse();
-  return (
-    mapCards.map((card) => (
-      <section key={card._id}>
-        <Container maxWidth="lg">
-          <Box pt={8} pb={10}>
-            <Grid container>
-              <Grid item xs={12} lg={6} className={classes.firstColumn}>
-                <Card className={classes.info}>
-                  <CardContent className={classes.info}>
-                    <Box
-                      display="flex"
-                      flexDirection="column"
-                      height="100%"
-                      pt={2}
-                      px={2}
-                    >
-                      <Typography variant="h5" component="h2" gutterBottom>
-                        {card.title}
+  const cards = props.cards.reverse();
+  const fillerImg = 'https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg';
+  return cards.map((card) => (
+    <section key={card._id}>
+      <Container maxWidth="lg">
+        <Box pt={8} pb={10}>
+          <Grid container>
+            <Grid item xs={12} lg={6} className={classes.firstColumn}>
+              <Card className={classes.info}>
+                <CardContent className={classes.info}>
+                  <Box
+                    display="flex"
+                    flexDirection="column"
+                    height="100%"
+                    pt={2}
+                    px={2}
+                  >
+                    <Typography variant="h5" component="h2" gutterBottom>
+                      {card.title}
+                    </Typography>
+                    <Box mt="auto" mb={2}>
+                      <Typography
+                        variant="body1"
+                        component="p"
+                        color="textSecondary"
+                      >
+                        {card.description}
                       </Typography>
-                      <Box mt="auto" mb={2}>
-                        <Typography
-                          variant="body1"
-                          component="p"
-                          color="textSecondary"
-                        >
-                          {card.description}
-                        </Typography>
-                      </Box>
-                      <Box textAlign="right">
-                        <Button color="primary" endIcon={<ArrowRightAltIcon />}>
-                          View this itinerary
-                        </Button>
-                      </Box>
                     </Box>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} lg={6}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} md={8}>
-                    <Card>
-                      <CardActionArea href="#">
-                        <CardMedia
-                          className={classes.media}
-                          image="https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg"
-                        />
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
-                  <Grid item xs={12} md={4}>
-                    <Card>
-                      <CardActionArea href="#">
-                        <CardMedia
-                          className={classes.media}
-                          image="https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg"
-                          alt="img"
-                        />
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
-                  <Grid item xs={12} md={4}>
-                    <Card>
-                      <CardActionArea href="#">
-                        <CardMedia
-                          className={classes.media}
-                          image="https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg"
-                          alt="imag"
-                        />
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
-                  <Grid item xs={12} md={8}>
-                    <Card>
-                      <CardActionArea href="#">
-                        <CardMedia
-                          className={classes.media}
-                          image="https://wttc.org/DesktopModules/MVC/NewsArticleList/images/141_20201013185512_Consumer%20Survey%20Finds%2070%20Percent%20of%20Travelers%20Plan%20to%20Holiday%20in%202021.jpg"
-                        />
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
+                    <Box textAlign="right">
+                      <Button color="primary" endIcon={<ArrowRightAltIcon />}>
+                        View this itinerary
+                      </Button>
+                    </Box>
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={8}>
+                  <Card>
+                    <CardActionArea href="#">
+                      <CardMedia
+                        className={classes.media}
+                        image={
+                          card.points[0].photo
+                            ? card.points[0].photo
+                            : fillerImg
+                        }
+                      />
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Card>
+                    <CardActionArea href="#">
+                      <CardMedia
+                        className={classes.media}
+                        image={
+                          card.points[1].photo
+                            ? card.points[1].photo
+                            : fillerImg
+                        }
+                        alt="img"
+                      />
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Card>
+                    <CardActionArea href="#">
+                      <CardMedia
+                        className={classes.media}
+                        image={
+                          card.points[2].photo
+                            ? card.points[2].photo
+                            : fillerImg
+                        }
+                        alt="imag"
+                      />
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                  <Card>
+                    <CardActionArea href="#">
+                      <CardMedia
+                        className={classes.media}
+                        image={
+                          card.points[3].photo
+                            ? card.points[3].photo
+                            : fillerImg
+                        }
+                      />
+                    </CardActionArea>
+                  </Card>
                 </Grid>
               </Grid>
             </Grid>
-          </Box>
-        </Container>
-      </section>
-    ))
-  );
+          </Grid>
+        </Box>
+      </Container>
+    </section>
+  ));
 }
